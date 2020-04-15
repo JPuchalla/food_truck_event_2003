582265df821cd7ee3e5e4a988d9db0f086f229c5
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-15 12:26:31 -0600

    it 2 up

--- lib/event.rb
+++ lib/event.rb
@@ -10,4 +10,15 @@ class Event
     @food_trucks << truck
   end
 
+  def food_truck_names
+    @food_trucks.map { |truck| truck.name }
+  end
+
+  def food_trucks_that_sell(item)
+    #doesnt work
+    @food_trucks.find_all do |truck|
+      if !truck.inventory[item].nil?
+      end
+    end
+  end
 end
